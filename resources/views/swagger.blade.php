4f3d00e247ad8e548257dbe9c27874e110417fef
AbdouAziz <abdouaziz583@gmail.com>
2024-09-23 10:51:58 +0000

    swagger

---
+++ resources/views/swagger.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="swagger-api"></div>
+    <script src="{{ mix('Apprenant/resources/js/swagger.js') }}"></script>
+
+    @vite('resources/js/swagger.js')    
+</body>
+</html>
--- routes/api.php
+++ routes/api.php
@@ -1,53 +1,28 @@
 <?php
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ReferentielController;
-use Laravel\Passport\Passport;
-use Kreait\Firebase\Factory;
-
+;
 
 Route::prefix('v1')->group(function () {
-    Route::post('/login', [AuthController::class, 'login'])->name('login'); // Login
-    Route::post('/logout', [AuthController::class, 'logout']); // Logout
-});
 
-Route::middleware(['auth:api', 'check.role:admin'])->group(function () {
-    Route::post('/users/forAdmin', [UserController::class, 'storeAdmin']);
-    Route::get('/users', [UserController::class, 'index']); // Route pour lister les utilisateurs
-    Route::get('/users/{id}', [UserController::class, 'show']); // Route pour afficher un utilisateur
-    Route::put('/users/{id}', [UserController::class, 'update']); // Route pour mettre à jour un utilisateur
+    // Auth Routes
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-});
-Route::prefix('v1')->group(function () {
+    // Protected Routes
     Route::middleware(['auth:api', 'check.role:admin,manager'])->group(function () {
-        Route::post('/referentiels', [ReferentielController::class, 'store']); // ajouter un Réferentiel 
-        Route::get('/referentiels', [ReferentielController::class, 'index']); // Lister tous les Réferentiel
-        Route::get('/referentiels/{id}', [ReferentielController::class, 'show']);// Afficher un référentiel avec ses compétences et modules
-        Route::get('/test', [ReferentielController::class, 'test']);
-        Route::patch('/referentiels/{id}', [ReferentielController::class, 'update']);
-        Route::delete('/referentiels/{id}', [ReferentielController::class, 'destroy']);
-        Route::get('/archive/referentiels', [ReferentielController::class, 'archived']);
+        // Referentiel Routes
+        Route::post('/referentiels', [ReferentielController::class, 'store'])->name('referentiels.store');
+        Route::get('/referentiels', [ReferentielController::class, 'index'])->name('referentiels.index');
+        Route::get('/referentiels/{id}', [ReferentielController::class, 'show'])->name('referentiels.show');
+        Route::patch('/referentiels/{id}', [ReferentielController::class, 'update'])->name('referentiels.update');
+        Route::delete('/referentiels/{id}', [ReferentielController::class, 'destroy'])->name('referentiels.destroy');
+        Route::get('/archive/referentiels', [ReferentielController::class, 'archived'])->name('referentiels.archived');
     });
-});
 
-Route::middleware(['auth:api', 'check.role:cm'])->group(function () {
-    Route::post('/users/forCm', [UserController::class, 'storeCm']);
-});
-
-Route::middleware(['auth:api', 'check.role:manager'])->group(function () {
-    Route::post('/users/forManager', [UserController::class, 'storeManager']);
-});
-
-Route::get('/test-firebase', function() {
-    $firebase = (new Factory)
-    ->withServiceAccount(config('firebase.credentials_file'))
-    ->withDatabaseUri(config('firebase.database_url'));
-
-    $database = $firebase->createDatabase();
-    $reference = $database->getReference('test_node');
-    $reference->set(['message' => 'Hello Firebase!']);
-
-    return response()->json(['message' => 'Firebase is connected and working!']);
+    Route::middleware(['auth:api', 'check.role:cm'])->post('/users/forCm', [UserController::class, 'storeCm']);
+    Route::middleware(['auth:api', 'check.role:manager'])->post('/users/forManager', [UserController::class, 'storeManager']);
 });
